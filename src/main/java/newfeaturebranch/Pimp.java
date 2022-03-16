fd9df05146cf4003287714bd7724af466f8f2904
mikailaAkeredolu <mikaila@tech552.com>
2022-03-16 13:17:55 -0400

    she got it

---
+++ src/main/java/newfeaturebranch/Pimp.java
@@ -0,0 +1,8 @@
+package newfeaturebranch;
+
+public class Pimp {
+
+    public static void main(String[] args) {
+        System.out.println("Cash Rules Err thing around me...dollar dollar billls y'alll!!!");
+    }
+}
