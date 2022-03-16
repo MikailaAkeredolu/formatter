fcaaecec0dc9aa7a57530e8bd3c47c16a966a9be
mikailaAkeredolu <mikaila@tech552.com>
2022-03-16 13:07:26 -0400

    pushing the demoish package and more

---
+++ src/main/java/demoforish/FakeNews.java
@@ -0,0 +1,8 @@
+package demoforish;
+
+public class FakeNews {
+
+    public static void main(String[] args) {
+        System.out.println("we gucci now..gang gang !!!!");
+    }
+}
--- src/main/java/formatter/Main.java
+++ src/main/java/formatter/Main.java
@@ -1,18 +1,26 @@
 package formatter;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String productName = "Mug";
-        int qty = 20;
-        float price = 5.0f;
-        float total = qty * price;
-        String s = String.format("%-15s %5s %10s\n", "Item", "Qty", "Price");
-        String s1 = String.format("%-15s %5s %10s\n", "----", "---", "-----");
-        String output = s + s1;
-        System.out.println(output);
-        String line = String.format("%-15s %5d %10.2f\n", productName, qty, price);
-        System.out.println(line);
-        String grandTotal = String.format("%-15s %5s %10.2f\n", "Total", " ", total);
-        System.out.println(grandTotal);
+
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter age");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("enter name");
+        String name = scanner.nextLine();
+
+        System.out.println("Got Covid");
+        boolean answer = scanner.nextBoolean();
+
+        System.out.println("Welcome "  + name + " It is " + answer+ " that you have covid" + " You can survive it since you are only" + age + " years old");
+
+
+
+
     }
 }
